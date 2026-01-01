59c8bb819821e71ac79e6baaf5df510dd39fea4e
mayarshawkyhamoda <hamodamayarshowkiy@gmail.com>
2026-01-01 21:12:39 +0200

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,73 @@
+var menuItems = [
+    {
+        name: "LASAL CHEESE",
+        price: "$18.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, explicabo.",
+        image: "./images/food1.png"
+    },
+    {
+        name: "LASAL CHEESE",
+        price: "$18.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, explicabo.",
+        image: "./images/food2.png"
+    },
+    {
+        name: "LASAL CHEESE",
+        price: "$18.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, explicabo.",
+        image: "./images/food3.png"
+    },
+    {
+        name: "LASAL CHEESE",
+        price: "$18.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, explicabo.",
+        image: "./images/food4.png"
+    },
+    {
+        name: "LASAL CHEESE",
+        price: "$18.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, explicabo.",
+        image: "./images/food5.png"
+    },
+    {
+        name: "LASAL CHEESE",
+        price: "$18.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, explicabo.",
+        image: "./images/food6.png"
+    },
+    {
+        name: "LASAL CHEESE",
+        price: "$18.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, explicabo.",
+        image: "./images/food7.jpeg"
+    },
+    {
+        name: "LASAL CHEESE",
+        price: "$18.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, explicabo.",
+        image: "./images/food8.jpeg"
+    },
+    {
+        name: "LASAL CHEESE",
+        price: "$18.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, explicabo.",
+        image: "./images/food9.jpeg"
+    },
+    {
+        name: "LASAL CHEESE",
+        price: "$18.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, explicabo.",
+        image: "./images/food10.jpeg"
+    },
+    {
+        name: "LASAL CHEESE",
+        price: "$18.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, explicabo.",
+        image: "./images/food11.jpeg"
+    },
+    {
+        name: "LASAL CHEESE",
+        price: "$18.00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, explicabo.",
+        image: "./images/food12.jpeg"
+    }]
