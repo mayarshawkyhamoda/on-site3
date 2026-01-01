99972dc486cb0d1ec865d8a6850b914d498fa264
mayarshawkyhamoda <hamodamayarshowkiy@gmail.com>
2026-01-01 21:24:31 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,118 +1,18 @@
-//////////////////////////////////////////////////////////////form validation////////////////////////////////////////////////////
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    var name = document.getElementById("name").value.trim();
-    var email = document.getElementById("email").value.trim();
-    var subject = document.getElementById("subject").value.trim();
-    var message = document.getElementById("message").value.trim(); 
-    var nameError = document.getElementById("name-error");
-    var emailError = document.getElementById("email-error");
-    var subjectError = document.getElementById("subject-error");
-    var messageError = document.getElementById("message-error");
+const toggle = document.getElementById("darkToggle");
+const html = document.documentElement;
+toggle.addEventListener('change', function () {
+    if (toggle.checked) {
+        html.classList.add('dark');
+        localStorage.setItem('theme', 'dark')
 
-    if (name === "") {
-        nameError.textContent = "Please enter your name.";
-        valid = false;
-    } else if (name.length < 3 || name.length > 15) {
-        nameError.textContent = "Name must be 3–15 characters.";
-        valid = false;
+    } else {
+        html.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
     }
 
-
-    if (email === "") {
-        emailError.textContent = "Please enter your email.";
-        valid = false;
-    }
-
-
-    if (subject !== "" && subject.length < 3) {
-        subjectError.textContent = "Subject must be at least 3 characters.";
-        valid = false;
-    }
-
-    if (message !== "" && message.length < 5) {
-        messageError.textContent = "Message must be at least 5 characters ";
-        valid = false;
-    }
-
-    if (valid) {
-        alert("Form submitted successfully!");
-  
-    }
 });
-var BtnClose = document.getElementById("close");
-var nextBtn = document.getElementById("next");
-var prevBtn = document.getElementById("prev");
-var BoxContainer = document.getElementById("boxContainer");
-var boxItem = document.getElementById("boxItem");
-var images = document.querySelectorAll(".img-gallery img");
+if (localStorage.getItem('theme') === 'dark') {
+    html.classList.add('dark');
+    toggle.checked = true
 
-var imagesArr = [];
-for (var i = 0; i < images.length; i++) {
-    imagesArr.push(images[i]);
 }
-var currentIndex = 0;
-
-function showImage(index) {
-    boxItem.style.backgroundImage = "url(" + imagesArr[index].src +
-        ")";
-}
- 
-for (var j = 0; j < imagesArr.length; j++) {
-    (function (j) {
-        imagesArr[j].addEventListener("click", function () {
-            BoxContainer.style.display = "flex";
-            currentIndex = j;
-            showImage(currentIndex);
-        });
-    })(j);
-}
-
-BtnClose.addEventListener("click", function () {
-    BoxContainer.style.display = "none";
-}); 
-
-nextBtn.addEventListener("click", function () {
-    currentIndex++;
-    if (currentIndex === imagesArr.length) {
-        currentIndex = 0;
-    }
-    showImage(currentIndex);
-});
-
-
-prevBtn.addEventListener("click", function () {
-    currentIndex--;
-    if (currentIndex < 0) {
-        currentIndex = imagesArr.length - 1;
-    }
-    showImage(currentIndex);
-});
-
-BoxContainer.addEventListener("click", function (event) {
-    if (event.target === BoxContainer) {
-        BoxContainer.style.display = "none";
-    }
-});
-
-
-
-
-
-
-let offers = document.querySelector('.offers-items');
-let next = document.querySelector('.next');
-let prev = document.querySelector('.prev');
-let index = 0;
-
-function slide(to) {
-    let length = offers.children.length;
-    index = (index + to + length) % length;
-    offers.style.transform = `translateX(-${index * 100}%)`;
-}
-
-next.onclick = () => slide(1);
-prev.onclick = () => slide(-1);
-
- 
-setInterval(() => slide(1), 5000);
